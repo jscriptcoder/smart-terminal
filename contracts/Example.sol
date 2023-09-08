b098588ea56dd53b5ea4ceab606bee246959b694
Francisco <jscriptcoder@gmail.com>
2023-09-09 00:33:55 +0200

    small change in the example

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.19;
 contract Example {
     string public name;
 
+    constructor(string memory _name) {
+        name = _name;
+    }
+
     function setName(string memory _name) public {
         name = _name;
     }
