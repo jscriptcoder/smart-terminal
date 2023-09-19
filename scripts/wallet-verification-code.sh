04ac47e0a22289f0abc5ebc43d46d1e12eda5d16
Francisco <jscriptcoder@gmail.com>
2023-09-19 15:14:03 +0200

    minor change

--- scripts/wallet-verification-code.sh
+++ scripts/wallet-verification-code.sh
@@ -2,10 +2,12 @@
 
 source .env
 
+walletconnect_txt="static/.well-known/walletconnect.txt"
+
 mkdir -p static/.well-known
 
-if [ ! -f static/.well-known/walletconnect.txt ]; then
-    touch static/.well-known/walletconnect.txt
+if [ ! -f $walletconnect_txt ]; then
+    touch $walletconnect_txt
 fi
 
-echo $WALLET_CONNECT_VERIFICATION_CODE > static/.well-known/walletconnect.txt
+echo $WALLET_CONNECT_VERIFICATION_CODE > $walletconnect_txt
